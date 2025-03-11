bbca9055b1bf22652cd5fa0a9e07b345177e69d0
Thota <jthota@mcw.edu>
2025-03-11 11:46:06 -0500

    updated definitions model

--- src/main/java/edu/mcw/scge/dao/implementation/DefinitionDAO.java
+++ src/main/java/edu/mcw/scge/dao/implementation/DefinitionDAO.java
@@ -2,6 +2,7 @@ package edu.mcw.scge.dao.implementation;
 
 import edu.mcw.scge.dao.AbstractDAO;
 import edu.mcw.scge.dao.spring.DefinitionQuery;
+import edu.mcw.scge.dao.spring.StringListQuery;
 import edu.mcw.scge.datamodel.Definition;
 
 import java.util.List;
@@ -17,4 +18,10 @@ public class DefinitionDAO extends AbstractDAO {
         DefinitionQuery query=new DefinitionQuery(this.getDataSource(), sql);
         return execute(query, category);
     }
+    public List<String> getAbbreviation(String term) throws Exception {
+        String sql=" select term from definitions where field=? and definition=?";
+       StringListQuery query=new StringListQuery(this.getDataSource(), sql);
+        return execute(query, "Abbreviation",term);
+    }
+
 }
