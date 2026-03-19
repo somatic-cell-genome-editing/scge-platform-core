209456684cafff778e1c4a995fe47347b54b41be
Akhilanand K <kundurthiakhil@gmail.com>
2026-03-19 11:09:08 -0500

    added pagination methods for chatbot curation

--- src/main/java/edu/mcw/scge/dao/implementation/DocumentEmbeddingDAO.java
+++ src/main/java/edu/mcw/scge/dao/implementation/DocumentEmbeddingDAO.java
@@ -8,6 +8,7 @@ import edu.mcw.scge.dao.spring.StringListQuery;
 import edu.mcw.scge.datamodel.DocumentEmbeddingSummary;
 
 import javax.sql.DataSource;
+import java.util.ArrayList;
 import java.util.List;
 
 public class DocumentEmbeddingDAO extends AbstractDAO {
@@ -43,6 +44,51 @@ public class DocumentEmbeddingDAO extends AbstractDAO {
 
     public boolean fileExists(String fileName) throws Exception {
         return getChunkCountByFileName(fileName) > 0;
+    }
+
+    public List<DocumentEmbeddingSummary> getFileSummariesPaginated(String type, String search, int limit, int offset) throws Exception {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT file_name, COUNT(*) as chunk_count, MIN(created_at) as uploaded_at FROM document_embeddings WHERE ");
+
+        List<Object> params = new ArrayList<>();
+
+        if ("clinicalTrials".equals(type)) {
+            sql.append("file_name LIKE 'CLINICAL TRIAL:%' ");
+        } else {
+            sql.append("file_name NOT LIKE 'CLINICAL TRIAL:%' ");
+        }
+
+        if (search != null && !search.trim().isEmpty()) {
+            sql.append("AND file_name ILIKE ? ");
+            params.add("%" + search.trim() + "%");
+        }
+
+        sql.append("GROUP BY file_name ORDER BY file_name LIMIT ? OFFSET ?");
+        params.add(limit);
+        params.add(offset);
+
+        DocumentEmbeddingSummaryQuery query = new DocumentEmbeddingSummaryQuery(this.getDataSource(), sql.toString());
+        return execute(query, params.toArray());
+    }
+
+    public int getFileCount(String type, String search) throws Exception {
+        StringBuilder sql = new StringBuilder();
+        sql.append("SELECT COUNT(DISTINCT file_name) FROM document_embeddings WHERE ");
+
+        List<Object> params = new ArrayList<>();
+
+        if ("clinicalTrials".equals(type)) {
+            sql.append("file_name LIKE 'CLINICAL TRIAL:%' ");
+        } else {
+            sql.append("file_name NOT LIKE 'CLINICAL TRIAL:%' ");
+        }
+
+        if (search != null && !search.trim().isEmpty()) {
+            sql.append("AND file_name ILIKE ? ");
+            params.add("%" + search.trim() + "%");
+        }
+
+        return getCount(sql.toString(), params.toArray());
     }
 
     public List<String> getChunksByFileName(String fileName) throws Exception {
